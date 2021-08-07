485ffa70bb8660c835db85da5180154143dd54fd
Ilyas Boukhechem <ilyas.boukhechem@gmx.fr>
2021-08-07 06:56:59 +0200

    change expired time for connection token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -93,7 +93,7 @@ exports.login = (req, res, next) => {
                 { userId: users.id, admin: users.admin },
                 secret.authSecret,
                 {
-                  expiresIn: "10s",
+                  expiresIn: "6h",
                 }
               ),
             });
